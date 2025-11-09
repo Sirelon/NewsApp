41067857b3d8abb858a8b163fe999eecbb60db45
sirelon <sasha.sirelon@gmail.com>
2025-11-09 11:23:19 +0200

    Add AppDimens.kt

---
+++ app/src/main/java/com/sirelon/newsapp/ui/theme/AppDimens.kt
@@ -0,0 +1,108 @@
+package com.sirelon.newsapp.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object AppDimens {
+
+    object Size {
+        val xs: Dp = 4.dp
+        val s: Dp = 6.dp
+        val m: Dp = 8.dp
+        val l: Dp = 10.dp
+        val xl: Dp = 12.dp
+        val xl2: Dp = 14.dp
+        val xl3: Dp = 16.dp
+        val xl4: Dp = 18.dp
+        val xl5: Dp = 20.dp
+        val xl6: Dp = 24.dp
+        val xl7: Dp = 28.dp
+        val xl8: Dp = 32.dp
+        val xl9: Dp = 36.dp
+        val xl10: Dp = 40.dp
+        val xl11: Dp = 44.dp
+        val xl12: Dp = 48.dp
+        val xl13: Dp = 56.dp
+        val xl14: Dp = 64.dp
+        val xl15: Dp = 72.dp
+        val xl16: Dp = 80.dp
+        val xl17: Dp = 88.dp
+        val xl18: Dp = 96.dp
+        val xl19: Dp = 108.dp
+        val xl20: Dp = 112.dp
+        val xl21: Dp = 128.dp
+        val xl22: Dp = 160.dp
+        val xl23: Dp = 240.dp
+    }
+
+    object Spacing {
+        val xs4: Dp = 0.dp
+        val xs3: Dp = 1.dp
+        val xs2: Dp = 2.dp
+        val xs: Dp = 4.dp
+        val s: Dp = 6.dp
+        val m: Dp = 8.dp
+        val l: Dp = 10.dp
+        val xl: Dp = 12.dp
+        val xl2: Dp = 14.dp
+        val xl3: Dp = 16.dp
+        val xl4: Dp = 18.dp
+        val xl5: Dp = 20.dp
+        val xl6: Dp = 24.dp
+        val xl7: Dp = 28.dp
+        val xl8: Dp = 32.dp
+        val xl9: Dp = 36.dp
+        val xl10: Dp = 40.dp
+        val xl11: Dp = 44.dp
+        val xl12: Dp = 48.dp
+        val xl13: Dp = 56.dp
+        val xl14: Dp = 64.dp
+        val xl15: Dp = 72.dp
+        val xl16: Dp = 84.dp
+        val xl17: Dp = 96.dp
+    }
+
+    object BorderRadius {
+        val xs4: Dp = 0.dp
+        val xs3: Dp = 1.dp
+        val xs2: Dp = 2.dp
+        val xs: Dp = 4.dp
+        val s: Dp = 6.dp
+        val m: Dp = 8.dp
+        val l: Dp = 10.dp
+        val xl: Dp = 12.dp
+        val xl2: Dp = 14.dp
+        val xl3: Dp = 16.dp
+        val xl4: Dp = 18.dp
+        val xl5: Dp = 20.dp
+        val xl6: Dp = 22.dp
+        val xl7: Dp = 24.dp
+        val xl8: Dp = 26.dp
+        val xl9: Dp = 28.dp
+        val xl10: Dp = 30.dp
+        val xl11: Dp = 32.dp
+    }
+
+    object BorderWidth {
+        val xs2: Dp = 0.3.dp
+        val xs: Dp = 0.5.dp
+        val s: Dp = 1.0.dp
+        val m: Dp = 1.5.dp
+        val l: Dp = 2.0.dp
+        val xl: Dp = 4.0.dp
+        val xl2: Dp = 8.0.dp
+    }
+
+    object TextSize {
+        val m: TextUnit = 8.sp
+        val l: TextUnit = 10.sp
+        val xl: TextUnit = 12.sp
+        val xl2: TextUnit = 14.sp
+        val xl3: TextUnit = 16.sp
+        val xl4: TextUnit = 18.sp
+        val xl5: TextUnit = 20.sp
+        val xl6: TextUnit = 24.sp
+    }
+}
